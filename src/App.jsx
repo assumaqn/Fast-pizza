0a958fc5e66d0c61838ca313e3d7f2233745106f
unknown <somegetiye@gmail.com>
2025-11-14 11:33:47 -0800

    building the app layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,22 @@ import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import New from "./features/order/CreateOrder";
 import OrderItem from "./features/order/OrderItem";
+import AppLayout from "./ui/appLayout";
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      { path: "/menu", element: <Menu /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order", element: <Order /> },
+      { path: "/order/new", element: <New /> },
+      { path: "/order:orderID", element: <OrderItem /> },
+    ],
   },
-  { path: "/menu", element: <Menu /> },
-  { path: "/cart", element: <Cart /> },
-  { path: "/order", element: <Order /> },
-  { path: "/order/new", element: <New /> },
-  { path: "/order:orderID", element: <OrderItem /> },
 ]);
 function App() {
   return <RouterProvider router={router} />;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Kumneger G</p>
+    </header>
+  );
+}
+
+export default Header;
