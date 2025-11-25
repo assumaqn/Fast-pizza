87157d9761f84f5688743be7674fcdfb84bf6315
unknown <somegetiye@gmail.com>
2025-11-25 10:48:20 -0800

    styling the cart page

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -5,12 +5,11 @@ function Button({ children, type, onClick, disabled, to }) {
   const base =
     "inline-block rounded-full bg-yellow-400  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
 
-  // const base =
-  //   "inline-block rounded-full bg-yellow-400 px-4 py-2 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:py-2";
-
   const style = {
     primary: base + " px-4 py-2 sm:py-2",
-    small: base + "  px-3 py-1 sm:py-3 sm:px-2 text-xs ",
+    small: base + "  px-4 py-2 md:py-2.5 md:px-5 text-xs ",
+    secondary:
+      "inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-2",
   };
 
   if (to)
